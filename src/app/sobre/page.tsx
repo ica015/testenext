f3d6eeb44087d67590f25633ac82d96edcc25918
Carlos Henrique <ica015@hotmail.com>
2023-10-24 20:42:09 -0300

    incluindo css na pagina sobre

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import styles from './sobre.module.css'
 
 export default function Sobre() {
   return (
 
     <div>
-      <h1>Sobre Nós</h1>
+      <h1 className={styles.margem}>Sobre Nós</h1>
       <br />
       <Link href={'/'}>
         <i>Página Inicial</i> 
