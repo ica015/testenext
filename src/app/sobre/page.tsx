c0367d8373f31f6f1f62933aea99e47d20ea78b3
Carlos Henrique <ica015@hotmail.com>
2023-10-24 19:59:32 -0300

    Inicio do projeto

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Sobre() {
+  return (
+
+    <div>
+      <h1>Sobre Nós</h1>
+      <br />
+      <Link href={'/'}>
+        <i>Página Inicial</i>
+      </Link>
+    </div>
+  )
+}
